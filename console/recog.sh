664f6990a3a9081d144b36b10f1223fbffab1058
Pranatadesta <rahmatullaharrizal@gmail.com>
2018-11-23 16:50:38 +0700

    Recognition

---
+++ console/recog.sh
@@ -0,0 +1,2 @@
+# HVite -A -D -C -T config2.conf 1 -H hmmsdef.mmf -i reco.mlf -w net.slf dict.txt hmmlist.txt test/12615050_insan_1.mfcc
+HVite -A -D -T 1 -C directin.conf -g -H hmmsdef.mmf -i reco.mlf -w net.slf dict.txt hmmlist.txt test/12615050_insan_1.mfcc
--- console/rest.sh
+++ console/rest.sh
@@ -1,6 +1,6 @@
-HRest -A -D -T 1 -C config2.conf -S trainlist.txt -M model/hmm1 -H model/hmm0flat -H model/hmm0/hmm_sil -L data/train/lab/ sil
-HRest -A -D -T 1 -C config2.conf -S trainlist.txt -M model/hmm1 -H model/hmm0flat -H model/hmm0/hmm_satu -L data/train/lab/ satu
-HRest -A -D -T 1 -C config2.conf -S trainlist.txt -M model/hmm1 -H model/hmm0flat -H model/hmm0/hmm_dua -L data/train/lab/ dua
-HRest -A -D -T 1 -C config2.conf -S trainlist.txt -M model/hmm1 -H model/hmm0flat -H model/hmm0/hmm_lima -L data/train/lab/ lima
-HRest -A -D -T 1 -C config2.conf -S trainlist.txt -M model/hmm1 -H model/hmm0flat -H model/hmm0/hmm_enam -L data/train/lab/ enam
+HRest -A -D -T 1 -C config2.conf -S trainlist.txt -M model/hmm1 -H model/hmm0flat -H model/hmm0/hmm_sil -L data/train/lab/ sil &&
+HRest -A -D -T 1 -C config2.conf -S trainlist.txt -M model/hmm1 -H model/hmm0flat -H model/hmm0/hmm_satu -L data/train/lab/ satu &&
+HRest -A -D -T 1 -C config2.conf -S trainlist.txt -M model/hmm1 -H model/hmm0flat -H model/hmm0/hmm_dua -L data/train/lab/ dua &&
+HRest -A -D -T 1 -C config2.conf -S trainlist.txt -M model/hmm1 -H model/hmm0flat -H model/hmm0/hmm_lima -L data/train/lab/ lima &&
+HRest -A -D -T 1 -C config2.conf -S trainlist.txt -M model/hmm1 -H model/hmm0flat -H model/hmm0/hmm_enam -L data/train/lab/ enam &&
 HRest -A -D -T 1 -C config2.conf -S trainlist.txt -M model/hmm1 -H model/hmm0flat -H model/hmm0/hmm_nol -L data/train/lab/ nol 
