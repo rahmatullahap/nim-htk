4a79ef627ddbdefd5088d724a00b3fec8f1c4853
Insan <insan@mecha.id>
2018-11-24 18:05:10 +0700

    add automation in init.sh & rest.sh, add audio_to_sig.sh

--- console/init.sh
+++ console/init.sh
@@ -1,7 +1,24 @@
-HInit -A -D -T 1 -S trainlist.txt -M model/hmm0 -H model/proto/hmm_sil -l sil -L data/train/lab/ sil &&
-HInit -A -D –T 1 -S trainlist.txt -M model/hmm0 -H model/proto/hmm_satu -l satu -L data/train/lab/ satu && 
-HInit -A -D –T 1 -S trainlist.txt -M model/hmm0 -H model/proto/hmm_dua -l dua -L data/train/lab/ dua &&
-HInit -A -D –T 1 -S trainlist.txt -M model/hmm0 -H model/proto/hmm_lima -l lima -L data/train/lab/ lima && 
-HInit -A -D –T 1 -S trainlist.txt -M model/hmm0 -H model/proto/hmm_enam -l enam -L data/train/lab/ enam &&
-HInit -A -D –T 1 -S trainlist.txt -M model/hmm0 -H model/proto/hmm_nol -l nol -L data/train/lab/ nol 
+#!/bin/bash
 
+for f in model/proto/hmm_*; do
+    # Create hmm0 directory if not exist
+    if [ ! -d model/hmm0 ];
+    then
+        mkdir model/hmm0
+    fi
+
+    # Train [0]
+    if [ -f "$f" ]
+    then
+        LABEL="${f/model\/proto\/hmm_/}"
+        echo $LABEL
+        HInit -A -D -T 1 -S trainlist.txt -M model/hmm0 -H $f -l $LABEL -L data/train/lab/ $LABEL
+    fi
+done
+
+# HInit -A -D -T 1 -S trainlist.txt -M model/hmm0 -H model/proto/hmm_sil -l sil -L data/train/lab/ sil &&
+# HInit -A -D -T 1 -S trainlist.txt -M model/hmm0 -H model/proto/hmm_satu -l satu -L data/train/lab/ satu && 
+# HInit -A -D -T 1 -S trainlist.txt -M model/hmm0 -H model/proto/hmm_dua -l dua -L data/train/lab/ dua &&
+# HInit -A -D -T 1 -S trainlist.txt -M model/hmm0 -H model/proto/hmm_lima -l lima -L data/train/lab/ lima && 
+# HInit -A -D -T 1 -S trainlist.txt -M model/hmm0 -H model/proto/hmm_enam -l enam -L data/train/lab/ enam &&
+# HInit -A -D -T 1 -S trainlist.txt -M model/hmm0 -H model/proto/hmm_nol -l nol -L data/train/lab/ nol 
--- console/rest.sh
+++ console/rest.sh
@@ -1,6 +1,32 @@
-HRest -A -D -T 1 -C config2.conf -S trainlist.txt -M model/hmm1 -H model/hmm0flat -H model/hmm0/hmm_sil -L data/train/lab/ sil &&
-HRest -A -D -T 1 -C config2.conf -S trainlist.txt -M model/hmm1 -H model/hmm0flat -H model/hmm0/hmm_satu -L data/train/lab/ satu &&
-HRest -A -D -T 1 -C config2.conf -S trainlist.txt -M model/hmm1 -H model/hmm0flat -H model/hmm0/hmm_dua -L data/train/lab/ dua &&
-HRest -A -D -T 1 -C config2.conf -S trainlist.txt -M model/hmm1 -H model/hmm0flat -H model/hmm0/hmm_lima -L data/train/lab/ lima &&
-HRest -A -D -T 1 -C config2.conf -S trainlist.txt -M model/hmm1 -H model/hmm0flat -H model/hmm0/hmm_enam -L data/train/lab/ enam &&
-HRest -A -D -T 1 -C config2.conf -S trainlist.txt -M model/hmm1 -H model/hmm0flat -H model/hmm0/hmm_nol -L data/train/lab/ nol 
+#!/bin/bash
+
+ITERATION=5
+for i in $(seq 1 $ITERATION); do
+    i_minus_1=$((i - 1))
+
+    PREV_MODEL_FOLDER="model/hmm${i_minus_1}"
+    # Create hmm0 directory if not exist
+    MODEL_FOLDER="model/hmm${i}"
+    if [ ! -d $MODEL_FOLDER ];
+    then
+        mkdir $MODEL_FOLDER
+    fi
+
+    for f in model/proto/hmm_*; do
+
+        # Train [0]
+        if [ -f "$f" ]
+        then
+            LABEL="${f/model\/proto\/hmm_/}"
+            echo "${MODEL_FOLDER} ${PREV_MODEL_FOLDER} ${LABEL}" 
+            HRest -A -D -T 1 -C config2.conf -S trainlist.txt -M $MODEL_FOLDER -H model/hmm0flat -H "${PREV_MODEL_FOLDER}/hmm_${LABEL}" -L data/train/lab/ $LABEL
+        fi
+    done
+done
+# HRest -A -D -T 1 -C config2.conf -S trainlist.txt -M model/hmm1 -H model/hmm0flat -H model/hmm0/hmm_sil -L data/train/lab/ sil
+# HRest -A -D -T 1 -C config2.conf -S trainlist.txt -M model/hmm1 -H model/hmm0flat -H model/hmm0/hmm_satu -L data/train/lab/ satu
+# HRest -A -D -T 1 -C config2.conf -S trainlist.txt -M model/hmm1 -H model/hmm0flat -H model/hmm0/hmm_dua -L data/train/lab/ dua
+# HRest -A -D -T 1 -C config2.conf -S trainlist.txt -M model/hmm1 -H model/hmm0flat -H model/hmm0/hmm_lima -L data/train/lab/ lima
+# HRest -A -D -T 1 -C config2.conf -S trainlist.txt -M model/hmm1 -H model/hmm0flat -H model/hmm0/hmm_enam -L data/train/lab/ enam
+# HRest -A -D -T 1 -C config2.conf -S trainlist.txt -M model/hmm1 -H model/hmm0flat -H model/hmm0/hmm_nol -L data/train/lab/ nol 
+
