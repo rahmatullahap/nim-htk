f20d64a46ecf9a4aa1beff45324276d5c92bad60
sari.rahmawati11@gmail.com <sari.rahmawati11@gmail.com>
2018-11-26 03:07:12 -0500

    tambah si

--- console/init.sh
+++ console/init.sh
@@ -16,9 +16,4 @@ for f in model/proto/hmm_*; do
     fi
 done
 
-# HInit -A -D -T 1 -S trainlist.txt -M model/hmm0 -H model/proto/hmm_sil -l sil -L data/train/lab/ sil &&
-# HInit -A -D -T 1 -S trainlist.txt -M model/hmm0 -H model/proto/hmm_satu -l satu -L data/train/lab/ satu && 
-# HInit -A -D -T 1 -S trainlist.txt -M model/hmm0 -H model/proto/hmm_dua -l dua -L data/train/lab/ dua &&
-# HInit -A -D -T 1 -S trainlist.txt -M model/hmm0 -H model/proto/hmm_lima -l lima -L data/train/lab/ lima && 
-# HInit -A -D -T 1 -S trainlist.txt -M model/hmm0 -H model/proto/hmm_enam -l enam -L data/train/lab/ enam &&
-# HInit -A -D -T 1 -S trainlist.txt -M model/hmm0 -H model/proto/hmm_nol -l nol -L data/train/lab/ nol 
+
