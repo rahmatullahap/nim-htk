f04b4eeed58674cdce752b5f71c212a3a9735779
Insan <insan@mecha.id>
2018-11-27 19:46:17 +0700

    fix initialize.py

--- final/python/initialize.py
+++ final/python/initialize.py
@@ -48,33 +48,6 @@ targetlist_filename = './txt/targetlist.txt'
 with open(targetlist_filename, 'w') as outfile:
     for line in targetlist_lines:
         outfile.write(line)
-        
-# BUILD TARGET LIST FOR TEST DATA
-print ('> BUILD TARGET LIST...')
-
-sig_directory = './data/test/sig/'
-mfcc_directory = './data/test/mfcc/'
-
-targetlist_lines = []
-trainlist_lines = []
-
-for filename in os.listdir(sig_directory):
-    if filename.endswith('.sig'):
-        filename_without_ext = filename.replace('.sig','')
-        
-        mfcc_filename = mfcc_directory.lstrip('./') + filename_without_ext + '.mfcc'
-        sig_filename = sig_directory.lstrip('./') + filename
-        
-        targetlist_line = sig_filename + ' ' + mfcc_filename + '\n'
-        targetlist_lines.append(targetlist_line)
-        
-        trainlist_line = mfcc_filename + '\n'
-        trainlist_lines.append(trainlist_line)
-
-targetlist_filename = './txt/test_targetlist.txt'
-with open(targetlist_filename, 'w') as outfile:
-    for line in targetlist_lines:
-        outfile.write(line)
 
 # BUILD TRAIN LIST
 print ('> BUILD TRAIN LIST...')
