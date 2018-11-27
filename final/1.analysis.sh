9b3c169103f65595c044bbbdf285d0adcabb45d0
Insan <insan@mecha.id>
2018-11-27 19:10:41 +0700

    add mfcc conversion for test data : initialize.py and analysis.sh

--- final/1.analysis.sh
+++ final/1.analysis.sh
@@ -1,1 +1,2 @@
 HCopy -A -D -C config/analysis.conf -S txt/targetlist.txt
+HCopy -A -D -C config/analysis.conf -S txt/test_targetlist.txt
--- final/python/initialize.py
+++ final/python/initialize.py
@@ -22,7 +22,7 @@ for filename in os.listdir(directory):
                         unique_labels.append(line_element[2])
 print ('UNIQUE LABELS:', unique_labels)
                         
-# BUILD TARGET LIST
+# BUILD TARGET LIST FOR TRAIN DATA
 print ('> BUILD TARGET LIST...')
 
 sig_directory = './data/train/sig/'
@@ -45,6 +45,33 @@ for filename in os.listdir(sig_directory):
         trainlist_lines.append(trainlist_line)
 
 targetlist_filename = './txt/targetlist.txt'
+with open(targetlist_filename, 'w') as outfile:
+    for line in targetlist_lines:
+        outfile.write(line)
+        
+# BUILD TARGET LIST FOR TEST DATA
+print ('> BUILD TARGET LIST...')
+
+sig_directory = './data/test/sig/'
+mfcc_directory = './data/test/mfcc/'
+
+targetlist_lines = []
+trainlist_lines = []
+
+for filename in os.listdir(sig_directory):
+    if filename.endswith('.sig'):
+        filename_without_ext = filename.replace('.sig','')
+        
+        mfcc_filename = mfcc_directory.lstrip('./') + filename_without_ext + '.mfcc'
+        sig_filename = sig_directory.lstrip('./') + filename
+        
+        targetlist_line = sig_filename + ' ' + mfcc_filename + '\n'
+        targetlist_lines.append(targetlist_line)
+        
+        trainlist_line = mfcc_filename + '\n'
+        trainlist_lines.append(trainlist_line)
+
+targetlist_filename = './txt/test_targetlist.txt'
 with open(targetlist_filename, 'w') as outfile:
     for line in targetlist_lines:
         outfile.write(line)
